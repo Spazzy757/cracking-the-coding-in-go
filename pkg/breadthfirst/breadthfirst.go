8ba942925bcedcd75fbebe4aac583a9a86520f18
Spazzy <brendankamp757@gmail.com>
2023-02-03 13:10:16 +0100

    chore: implement breadth first search logic
    Signed-off-by: Spazzy <brendankamp757@gmail.com>

--- pkg/breadthfirst/breadthfirst.go
+++ pkg/breadthfirst/breadthfirst.go
@@ -4,16 +4,72 @@
  */
 package breadthfirst
 
+type State int
+
+const (
+	Unvisited State = iota
+	Visited
+	Visiting
+)
+
 type Node struct {
 	id       string
-	state    string
-	adjacent []Node
+	State    string
+	Adjacent []Node
 }
 
-func search(start Node, end Node) Bool {
+type Queue struct {
+	List[Node]
+}
+
+func (q *Queue) IsEmpty() {
+	return len(q.List) == 0
+}
+
+func (q *Queue) Append(n Node) {
+	q.List = append(q.List, n)
+}
+
+func (q *Queue) Pop() Node {
+	if q.IsEmpty() {
+		panic("cant pop an empty list")
+	}
+
+	u := q.List[0]
+	q.List = q.List[1:]
+
+	return u
+}
+
+func Search(start Node, end Node) Bool {
 	if start == end {
 		return true
 	}
 
-	var nodeList []Node
+	// used as a queue
+	var q Queue
+
+	start.State
+	q = q.Append(start)
+
+	var u Node
+
+	for {
+		u = q.Pop()
+		for _, v := range u.Adjacent {
+			if v.State == Unvisited {
+				if v == end {
+					return true
+				} else {
+					v.State = Visting
+					q.Append(v)
+				}
+			}
+		}
+
+		if q.IsEmpty() {
+			break
+		}
+	}
+	return false
 }
